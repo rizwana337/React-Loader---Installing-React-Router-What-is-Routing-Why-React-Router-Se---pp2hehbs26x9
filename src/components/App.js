c61e53db2625ade894d93a7ebd9e5c42b6f93aeb
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-03 20:48:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,16 +11,26 @@ const LoadingStatus = {
 const App = () => {
   const BASE_URL = "https://content.newtonschool.co/v1/pr/main/users";
   const [userId, setUserId] = React.useState(1);
-  const [isLoading, setIsLoading] = React.useState(LoadingStatus.NOT_STARTED);
+  const [loading, setLoading] = React.useState(false);
   const [userData, setUserData] = React.useState({
     id: "",
     email: "",
     name: "",
     phone: "",
-    webiste: "",
+    website: "",
   });
 
-  const handleOnClick = () => {};
+  const handleOnClick = () => {
+    setLoading(true);
+    setTimeout(() => {
+      fetch(`${BASE_URL}/${userId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setLoading(false);
+          setUserData(data);
+        });
+    }, 2000);
+  };
 
   const onChangeHandler = (event) => {
     setUserId(event.target.value);
@@ -42,15 +52,79 @@ const App = () => {
       </button>
 
       <div id="data">
-        <h1>Click on the button to get the user</h1>
-        <h4 id="id">{userData.id}</h4>
-        <h4 id="email">{userData.email}</h4>
-        <h4 id="name">{userData.name}</h4>
-        <h4 id="phone">{userData.phone}</h4>
-        <h4 id="website">{userData.website}</h4>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <h1 id="id">{userData.id}</h1>
+            <h1 id="email">{userData.email}</h1>
+            <h1 id="name">{userData.name}</h1>
+            <h1 id="phone">{userData.phone}</h1>
+            <h1 id="website">{userData.website}</h1>
+          </>
+        )}
       </div>
     </div>
   );
 };
 
 export default App;
+
+
+
+// import React from "react";
+// import "../styles/App.css";
+// import Loader from "./Loader";
+
+// const LoadingStatus = {
+//   NOT_STARTED: "NOT_STARTED",
+//   IN_PROGRESS: "IN_PROGRESS",
+//   SUCCESS: "SUCCESS",
+// };
+
+// const App = () => {
+//   const BASE_URL = "https://content.newtonschool.co/v1/pr/main/users";
+//   const [userId, setUserId] = React.useState(1);
+//   const [isLoading, setIsLoading] = React.useState(LoadingStatus.NOT_STARTED);
+//   const [userData, setUserData] = React.useState({
+//     id: "",
+//     email: "",
+//     name: "",
+//     phone: "",
+//     webiste: "",
+//   });
+
+//   const handleOnClick = () => {};
+
+//   const onChangeHandler = (event) => {
+//     setUserId(event.target.value);
+//   };
+
+//   return (
+//     <div id="main">
+//       <label htmlFor="number">Enter an id for the user between 1 to 100</label>
+//       <input
+//         type="number"
+//         value={userId}
+//         onChange={onChangeHandler}
+//         id="input"
+//         min={1}
+//         max={10}
+//       />
+//       <button id="btn" onClick={handleOnClick}>
+//         Get User
+//       </button>
+
+//       <div id="data">
+//         <h1>Click on the button to get the user</h1>
+//         <h4 id="id">{userData.id}</h4>
+//         <h4 id="email">{userData.email}</h4>
+//         <h4 id="name">{userData.name}</h4>
+//         <h4 id="phone">{userData.phone}</h4>
+//         <h4 id="website">{userData.website}</h4>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
